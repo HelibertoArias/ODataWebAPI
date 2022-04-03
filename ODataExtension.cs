92275f2eb891912bae532d279493a94b72d7ca55
Heliberto Arias <helibertoarias@gmail.com>
2022-04-03 10:09:59 -0500

    Add project files.

---
+++ ODataExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+
+namespace ODataWebAPI
+{
+    public static class ODataExtension
+    {
+        public   static IEdmModel GetEdmModel()
+        {
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Book>("Books");
+            builder.EntitySet<Press>("Presses");
+            return builder.GetEdmModel();
+        }
+    }
+}
